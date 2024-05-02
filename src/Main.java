27979404495453a911f5b4dadf247002957101c3
Goran <goranbubalo2422003@gmail.com>
2024-05-02 18:54:50 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// Wrapper class = = > provides a way to use primitive data types as reference
+		// data type
+		// reference data types contain useful methods
+		// can be used with collections (ex. ArrayList)
+
+		// Primitive //Wrapper
+		// _______________________
+
+		// boolean 	Boolean
+		// char 	Character
+		// int 		Integer
+		// double 	Double
+
+		// autoboxing = > automatic conversion that the java compiler makes between
+		// the primitive types and their corresponding object wrapper classes
+
+		// unboxing = > reverse of autoboxing. Automatic conversion of wrapper class to
+		// primitive
+
+		
+		//U can still treat them like primitive data types 
+		Boolean a = true;
+		Character b = '@';
+		Integer c = 123;
+		Double d = 3.14;
+		String e = "Bro";
+		
+		if(a == true)
+		{
+			System.out.println("This is true");
+		}
+
+	}
+
+}
